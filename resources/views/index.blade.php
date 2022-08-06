aa7e137acd9b290f52dbcbad25fc6ddd8c3bf2a2
Khusela Mphokeli <innothetechgeek@gmail.com>
2022-08-07 00:42:01 +0200

    added modal to display and play course intro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,6 +39,30 @@
 
     gtag('config', 'G-3RZKY9576B');
   </script>
+
+  <style>
+    .modal-dialog {
+      max-width: 800px;
+      margin: 30px auto;
+    }
+
+    .modal-body {
+      position:relative;
+      padding:0px;
+    }
+    .close {
+      position:absolute;
+      right:-30px;
+      top:0;
+      z-index:999;
+      font-size:2rem;
+      font-weight: normal;
+      color:#fff;
+      opacity:1;
+    }
+  
+  </style>
+
 </head>
 
 <body class="theme-dark">
@@ -123,7 +147,11 @@
               If you're interested in this course click the join waiting list button below, you'll be notified via email when I release the course.
             
             </p>
-            <a class="btn content-work button-main" data-scroll="" href="#portfolio-area" role="button">Watch course intro</a>
+
+         
+
+
+            <a class="btn content-work button-main video-btn"    data-toggle="modal" data-src="https://www.youtube.com/watch?v=20A7oGWWRso&t=561s&autoplay=1&loop=1" data-target="#myModal">Watch course intro</a>
             <a class="btn content-hire button-main button-scheme" data-scroll="" href="#contact-area" role="button">Join waiting list</a>
           </div><a href="#about-area" data-scroll="">
             <div class="home-tip">More about me</div>
@@ -841,6 +869,46 @@
           <p class="brand-description">Web Developer, tech nerd and computer geek.</p>
         </div>
       </div>
+
+      <div class="container">
+    <h1>Play YouTube or Vimeo Videos in Bootstrap 4 Modal</h1>
+  
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+
+        
+        <div class="modal-body">
+
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>        
+            <!-- 16:9 aspect ratio -->
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
+            </div>
+          
+        </div>
+
+      </div>
+    </div>
+  </div> 
+    
+    
+    
+  </div>
+
+
+
+
+
+
+
+
+
+
       <!-- Mini footer-->
       <div class="mini-footer">
         <p class="copyright-notice">Copyright © 2022 Ela, all rights reserved.</p>
@@ -858,6 +926,41 @@
   <script src="{{ asset('template/js/main.js') }}"></script>
   <!-- Custom JS (Add your custom JS scripts to this file)-->
   <script src="{{ asset('template/js/custom.js') }}"></script>
+
+  <script>
+      $(document).ready(function() {
+
+      // Gets the video src from the data-src on each button
+
+      var $videoSrc;  
+      $('.video-btn').click(function() {
+          $videoSrc = $(this).data( "src" );
+      });
+      console.log($videoSrc);
+
+        
+        
+      // when the modal is opened autoplay it  
+      $('#myModal').on('shown.bs.modal', function (e) {
+          
+      // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+      $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+
+      })
+        
+
+
+      // stop playing the youtube video when I close the modal
+      $('#myModal').on('hide.bs.modal', function (e) {
+          // a poor man's stop video
+          $("#video").attr('src',$videoSrc); 
+      })  
+        
+      // document ready  
+      });
+        
+
+  </script>
 </body>
 
 </html>
